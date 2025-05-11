157e0fba57523bb0b4983460c5be1aa92d95746c
KalebIsaias <kiscstudies@gmail.com>
2025-05-11 11:38:36 -0300

    feat: adiciona configuracao inicial

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.FlatConfig} */
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": eslintPluginTs,
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+  },
+];
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { soma } from './index';
+
+test('soma 2 + 2 = 4', () => {
+  expect(soma(2, 2)).toBe(4);
+});
